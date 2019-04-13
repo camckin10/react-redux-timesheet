03fd6e4f89a2d81a788dfab37e3ef7289a431e59
Chelsey McKinney <chelseymckinney@gmail.com>
2019-04-13 13:32:57 -0500

    adding routes, navigation bar

--- lab-03/src/employees/EmployeeTable.test.js
+++ lab-03/src/employees/EmployeeTable.test.js
@@ -4,4 +4,23 @@ import { shallow } from 'enzyme';
 import EmployeeRow from './EmployeeRow';
 
 describe('<EmployeeTable />', () => {
+	let wrapper;
+
+	beforeEach(()=> {
+		const employees = [
+			{
+				username:'Username',
+				email: 'Email',
+				firstName: 'FirstName',
+				lastName:'LastName',
+				admin: true,
+				_id:1
+			}
+		];
+		wrapper = shallow(<EmployeeTable employees={employees} />);
+	});
+		it('should render a row for each employee', () => {
+    expect(wrapper.find(EmployeeRow)).toHaveLength(1);
+  });
+
 });
